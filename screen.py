c13507d8d61a28d0df88d7954e2b9c3d8798957f
ToucanMarooney <ToucanMarooney@gmail.com>
2016-01-30 12:57:07 +1030

    Removed logging and fixed constant naming.

--- screen.py
+++ screen.py
@@ -6,43 +6,7 @@ import time
 
 import terminator
 
-LOGFILE = "logfile.log"
-
-
-def get_datetime():
-    return datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-
-
-def new_logging_session(path):
-    """
-    Used to distinguish between different logging sessions.
-
-    :param path: path of the log file.
-    :return:
-    """
-    logfile = open(path, "a+")
-
-    if logfile.read().strip() == "":
-        logfile.write("{} New {} session, logging started.\n".format(get_datetime(), __name__))
-
-    else:
-        logfile.write("\n\n{} New {} session, logging started.\n".format(get_datetime(), __name__))
-
-    logfile.close()
-
-
-new_logging_session(LOGFILE)
-
-CONFIG_LEVEL = logging.DEBUG
-
-logging.basicConfig(level=CONFIG_LEVEL,
-                    format='%(asctime)s\n%(message)s',
-                    handlers=[logging.FileHandler(LOGFILE),
-                              logging.StreamHandler()])
-
-logger = logging.getLogger(__name__)
-
-terminal_width = 80
+TERMINAL_WIDTH = 80
 clear_command = "clear"
 
 if os.name == "nt":
@@ -87,20 +51,17 @@ class Screen(object):
         self.func_args = args
 
     def __call__(self, kwargs={}):
-        logger.debug("{}.__call__ called with args: %s".format(self), kwargs)
-
         WINDOW.clear_screen()
         to_display = self.information
 
         for key in kwargs.keys():
             to_display = to_display.replace("@" + key + "@", kwargs[key])
 
-        WINDOW.print(self.title_display.center(terminal_width))
+        WINDOW.print(self.title_display.center(TERMINAL_WIDTH))
         WINDOW.print()
         WINDOW.print(to_display)
 
         if len(self.func_args) > 0:
-            logger.debug("{}.function called with args: %s".format(self), self.func_args)
             return self.function(*self.func_args)
 
         else:
